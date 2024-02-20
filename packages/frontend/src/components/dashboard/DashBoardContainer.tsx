49a9199a83a5315ae7148987c68e69d649e0c1a7
CLV\minh.nlt <minh.nlt@cyberlogitec.com>
2024-02-20 13:29:02 +0700

    minor update chart

--- packages/frontend/src/components/dashboard/DashBoardContainer.tsx
+++ packages/frontend/src/components/dashboard/DashBoardContainer.tsx
@@ -2,19 +2,17 @@ import React from "react";
 import PieChartContainer from "./PieChartContainer";
 import BarChartContainer from "./BarChartContainer";
 import PolarAreaChartContainer from "./PolarAreaChartContainer";
-import DonutChartContainer from "./DonutChartContainer";
 import LineChartContainer from "./LineChartContainer";
-import RadarChartContainer from "./RadarChartContainer";
 
 function DashBoardContainer() {
   return (
-    <div className="grid grid-cols-4 items-center ">
-      <BarChartContainer />
+    <div className="grid grid-cols-2">
       <PolarAreaChartContainer />
       <PieChartContainer />
-      <DonutChartContainer />
+      {/* <DonutChartContainer /> */}
       <LineChartContainer />
-      <RadarChartContainer />
+      <BarChartContainer />
+      {/* <RadarChartContainer /> */}
     </div>
   );
 }
